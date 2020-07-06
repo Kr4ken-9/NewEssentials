b7ba953a3127e961e8eefdb106b62024d2574470
Kr4ken-9 <kr4kenkt@gmail.com>
2020-07-06 11:27:19 -0500

    /clear vehicles

---
+++ NewEssentials/Commands/Clear/CClearRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using OpenMod.Core.Commands;
+using Microsoft.Extensions.Localization;
+using OpenMod.API.Permissions;
+
+namespace NewEssentials.Commands.Clear
+{
+    [UsedImplicitly]
+    [Command("clear")]
+    [CommandDescription("description")]
+    [CommandSyntax("<items/vehicles/inventory>")]
+    public class CClearRoot : Command
+    {
+        private readonly IPermissionChecker m_PermissionChecker;
+
+        public CClearRoot(IPermissionChecker permissionChecker, IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            m_PermissionChecker = permissionChecker;
+        }
+
+        protected override async Task OnExecuteAsync()
+        {
+            string permission = "newess.clear";
+            if (await m_PermissionChecker.CheckPermissionAsync(Context.Actor, permission) == PermissionGrantResult.Deny)
+                throw new NotEnoughPermissionException(Context, permission);
+
+            throw new CommandWrongUsageException(Context);
+        }
+    }
+}
---
+++ NewEssentials/Commands/Clear/CClearVehicles.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+using OpenMod.Core.Commands;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using OpenMod.API.Commands;
+using OpenMod.API.Permissions;
+using SDG.Unturned;
+using Command = OpenMod.Core.Commands.Command;
+
+namespace NewEssentials.Commands.Clear
+{
+    [UsedImplicitly]
+    [Command("vehicles")]
+    [CommandAlias("vehicle")]
+    [CommandParent(typeof(CClearRoot))]
+    [CommandDescription("Clears all vehicles or only empty vehicles")]
+    [CommandSyntax("[empty]")]
+    public class CClearVehicles : Command
+    {
+        private readonly IPermissionChecker m_PermissionChecker;
+        private readonly IStringLocalizer m_StringLocalizer;
+
+        public CClearVehicles(IPermissionChecker permissionChecker, IStringLocalizer stringLocalizer, IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            m_PermissionChecker = permissionChecker;
+            m_StringLocalizer = stringLocalizer;
+        }
+
+        protected override async Task OnExecuteAsync()
+        {
+            string permission = "command.permission";
+            if (await m_PermissionChecker.CheckPermissionAsync(Context.Actor, permission) == PermissionGrantResult.Deny)
+                throw new NotEnoughPermissionException(Context, permission);
+
+            if (Context.Parameters.Length > 1)
+                throw new CommandWrongUsageException(Context);
+
+            if (Context.Parameters.Length == 0)
+            {
+                await UniTask.SwitchToMainThread();
+                VehicleManager.askVehicleDestroyAll();
+                await Context.Actor.PrintMessageAsync(m_StringLocalizer["clear:vehicles"]);
+            }
+            else
+            {
+                string notEmpty = Context.Parameters[0];
+                if(!notEmpty.Equals("empty", StringComparison.InvariantCultureIgnoreCase))
+                    throw new CommandWrongUsageException(Context);
+                
+                await UniTask.SwitchToMainThread();
+
+                byte counter = 0;
+                for (int index = VehicleManager.vehicles.Count - 1; index >= 0; index--)
+                {
+                    var vehicle = VehicleManager.vehicles[index];
+                    if (!vehicle.isEmpty)
+                        continue;
+                    
+                    VehicleManager.askVehicleDestroy(vehicle);
+                    counter++;
+                }
+
+                await Context.Actor.PrintMessageAsync(m_StringLocalizer["clear:vehicles_empty", new {Count = counter}]);
+            }
+        }
+    }
+}
