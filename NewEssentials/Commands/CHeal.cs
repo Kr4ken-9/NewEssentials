166d22445a5840e34283df35d4a0453dc21856de
Kr4ken-9 <kr4kenkt@gmail.com>
2020-07-07 12:26:53 -0500

    /heal

---
+++ NewEssentials/Commands/CHeal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+using OpenMod.Core.Commands;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using OpenMod.API.Commands;
+using OpenMod.API.Permissions;
+using OpenMod.Unturned.Users;
+using SDG.Unturned;
+using Command = OpenMod.Core.Commands.Command;
+
+namespace NewEssentials.Commands
+{
+    [UsedImplicitly]
+    [Command("heal")]
+    [CommandDescription("Heal yourself or another player")]
+    [CommandSyntax("[player]")]
+    public class CHeal : Command
+    {
+        private readonly IPermissionChecker m_PermissionChecker;
+        private readonly IStringLocalizer m_StringLocalizer;
+
+        public CHeal(IPermissionChecker permissionChecker, IStringLocalizer stringLocalizer, IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            m_PermissionChecker = permissionChecker;
+            m_StringLocalizer = stringLocalizer;
+        }
+
+        protected override async Task OnExecuteAsync()
+        {
+            string permission = "newess.heal";
+            if (await m_PermissionChecker.CheckPermissionAsync(Context.Actor, permission) == PermissionGrantResult.Deny)
+                throw new NotEnoughPermissionException(Context, permission);
+
+            if (Context.Parameters.Length > 1)
+                throw new CommandWrongUsageException(Context);
+
+            if (Context.Parameters.Length == 0)
+            {
+                UnturnedUser uPlayer = (UnturnedUser) Context.Actor;
+
+                await UniTask.SwitchToMainThread();
+                uPlayer.Player.life.askHeal(100, true, true);
+                await uPlayer.PrintMessageAsync(m_StringLocalizer["heal:success"]);
+            }
+            else
+            {
+                string searchTerm = Context.Parameters[0];
+                if (!PlayerTool.tryGetSteamPlayer(searchTerm, out SteamPlayer recipient))
+                    throw new UserFriendlyException(m_StringLocalizer["general:invalid_player",
+                        new {Player = searchTerm}]);
+
+                await UniTask.SwitchToMainThread();
+                recipient.player.life.askHeal(100, true, true);
+                await Context.Actor.PrintMessageAsync(m_StringLocalizer["heal:success_other", new {Player = recipient.playerID.characterName}]);
+            }
+        }
+    }
+}
