bedd9425e0cc47c3e81ac0ba1a41333099901a11
archie426 <archiestride42@gmail.com>
2020-07-16 11:21:02 +0100

    Improve effect

--- NewEssentials/Commands/CEffect.cs
+++ NewEssentials/Commands/CEffect.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using OpenMod.API.Permissions;
 using OpenMod.Core.Commands;
@@ -12,6 +13,7 @@ namespace NewEssentials.Commands
     [Command("effect")]
     [CommandAlias("eff")]
     [CommandDescription("Spawn an effect on your position")]
+    [CommandSyntax("<id> [player]")]
     public class CEffect : UnturnedCommand
     {
 
@@ -24,18 +26,40 @@ namespace NewEssentials.Commands
 
         protected override async UniTask OnExecuteAsync()
         {
-            //TODO: Add support for other players, configurable allowed effects?
-
+            //TODO: Add support for configurable allowed effects?
+            
             if (await _permission.CheckPermissionAsync(Context.Actor, "effect") != PermissionGrantResult.Grant)
                 throw new NotEnoughPermissionException(Context, "You do not have permission to execute this command!");
 
-            ushort effectId = await Context.Parameters.GetAsync<ushort>(0);
-
             UnturnedUser usr = (UnturnedUser) Context.Actor;
+            Player target;
 
+            
+            if (Context.Parameters.Length > 1 && Context.Parameters.Length < 2)
+            {
+                target = Provider.clients.FirstOrDefault(p =>
+                    p.player.name.Contains(Context.Parameters.GetAsync<string>(1).Result))?.player;
+                
+                if (target == null)
+                    throw new CommandWrongUsageException("Could not find player");
+            }
+            else if (Context.Parameters.Length == 1)
+                target = ((UnturnedUser) Context.Actor).Player;
+            else
+                throw new CommandWrongUsageException("Unexpected amount of parameters");
+
+
+            if (target == null)
+                return;
+            
+            ushort effectId = await Context.Parameters.GetAsync<ushort>(0);
+            
             await UniTask.SwitchToMainThread();
             
-            EffectManager.sendEffect(effectId, usr.SteamId, usr.Player.transform.position);
+            EffectManager.sendEffect(effectId, target.channel.owner.playerID.steamID, target.transform.position);
+            
+            await usr.PrintMessageAsync(
+                $"Successfully gave effect {effectId} to {target.channel.owner.playerID.characterName}");
         }
     }
 }
