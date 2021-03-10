cdc6a55de57fafa80c60edf344bb9aa41824a0f7
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-03-10 13:50:54 +0700

    Fix godmode

--- NewEssentials/Players/GodManager.cs
+++ NewEssentials/Players/GodManager.cs
@@ -11,17 +11,18 @@ using System.Threading.Tasks;
 namespace NewEssentials.Players
 {
     [PluginServiceImplementation(Lifetime = ServiceLifetime.Singleton, Priority = Priority.Normal)]
-    public class GodManager : IGodManager
+    public class GodManager : IGodManager, IDisposable
     {
         private readonly HashSet<ulong> m_Gods;
-        
+        private readonly IDisposable m_EventListener;
+
         //TODO: Add some harmony patches to prevent all damage e.g infection/dehydration/suffocation since Nelly selectively uses this event
         public GodManager(NewEssentials plugin,
             IEventBus eventBus)
         {
             m_Gods = new HashSet<ulong>();
 
-            eventBus.Subscribe(plugin, (EventCallback<UnturnedPlayerDamagingEvent>)OnPlayerDamaging);
+            m_EventListener = eventBus.Subscribe<UnturnedPlayerDamagingEvent>(plugin, OnPlayerDamaging);
         }
 
         public bool ToggleGod(ulong steamID)
@@ -38,10 +39,14 @@ namespace NewEssentials.Players
 
         private Task OnPlayerDamaging(IServiceProvider serviceProvider, object sender, UnturnedPlayerDamagingEvent @event)
         {
-            if (m_Gods.Contains(@event.Killer.m_SteamID))
-                @event.IsCancelled = true;
+            @event.IsCancelled = m_Gods.Contains(@event.Player.SteamId.m_SteamID);
 
             return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            m_EventListener?.Dispose();
         }
     }
 }
