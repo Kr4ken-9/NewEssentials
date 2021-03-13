1d7bfc7ad3ba5d0e8e74ccb121ea2aacadd09f16
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-03-13 22:35:58 +0700

    CRefuel: Add refuelable object support

--- NewEssentials/Commands/CRefuel.cs
+++ NewEssentials/Commands/CRefuel.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Cysharp.Threading.Tasks;
-using OpenMod.Core.Commands;
+﻿using Cysharp.Threading.Tasks;
 using Microsoft.Extensions.Localization;
 using OpenMod.API.Commands;
+using OpenMod.Core.Commands;
 using OpenMod.Unturned.Commands;
 using OpenMod.Unturned.Users;
 using SDG.Unturned;
-using UnityEngine;
+using System;
 
 namespace NewEssentials.Commands
 {
@@ -27,7 +26,7 @@ namespace NewEssentials.Commands
             if (Context.Parameters.Length != 0)
                 throw new CommandWrongUsageException(Context);
 
-            UnturnedUser uPlayer = (UnturnedUser) Context.Actor;
+            UnturnedUser uPlayer = (UnturnedUser)Context.Actor;
             await UniTask.SwitchToMainThread();
 
             var currentVehicle = uPlayer.Player.Player.movement.getVehicle();
@@ -36,64 +35,81 @@ namespace NewEssentials.Commands
                 if (!RefuelVehicle(currentVehicle))
                     throw new UserFriendlyException(m_StringLocalizer["refuel:vehicle:no_fuel"]);
 
-                await uPlayer.PrintMessageAsync(m_StringLocalizer["refuel:vehicle:success"]);
+                await PrintAsync(m_StringLocalizer["refuel:vehicle:success"]);
                 return;
             }
-            
+
             PlayerLook look = uPlayer.Player.Player.look;
-            RaycastInfo raycast = DamageTool.raycast(new Ray(look.aim.position, look.aim.forward),
-                100f, RayMasks.VEHICLE | RayMasks.BARRICADE);
+            RaycastInfo raycast = DamageTool.raycast(new(look.aim.position, look.aim.forward), 100f, RayMasks.DAMAGE_SERVER);
 
             if (raycast.vehicle != null)
             {
                 if (!RefuelVehicle(raycast.vehicle))
                     throw new UserFriendlyException(m_StringLocalizer["refuel:vehicle:no_fuel"]);
 
-                await uPlayer.PrintMessageAsync(m_StringLocalizer["refuel:vehicle:success"]);
+                await PrintAsync(m_StringLocalizer["refuel:vehicle:success"]);
                 return;
             }
 
             if (raycast.transform == null)
+            {
                 throw new UserFriendlyException(m_StringLocalizer["refuel:none"]);
-
-            var generator = raycast.transform.GetComponentInChildren<InteractableGenerator>();
-            if (generator != null)
-            {
-                if (!generator.isRefillable)
-                    throw new UserFriendlyException(m_StringLocalizer["refuel:object:no_fuel",
-                        new {Object = generator.name}]);
-                
-                generator.askFill(generator.capacity);
-                
-                BarricadeManager.sendFuel(raycast.transform, generator.fuel);
-                await uPlayer.PrintMessageAsync(m_StringLocalizer["refuel:object:generator"]);
-                return;
             }
 
-            var oil = raycast.transform.GetComponentInChildren<InteractableOil>();
-            if (oil != null)
+            var interactable = raycast.transform.GetComponent<Interactable>();
+            if (interactable != null)
             {
-                if (!oil.isRefillable)
-                    throw new UserFriendlyException(m_StringLocalizer["refuel:object:no_fuel",
-                        new {Object = oil.name}]);
-                
-                oil.askFill(oil.capacity);
-                
-                BarricadeManager.sendFuel(raycast.transform, oil.fuel);
-                await uPlayer.PrintMessageAsync(m_StringLocalizer["refuel:object:oil"]);
-                return;
-            }
-            
-            var tank = raycast.transform.GetComponentInChildren<InteractableTank>();
-            if (tank != null)
-            {
-                if (!tank.isRefillable)
-                    throw new UserFriendlyException(
-                        m_StringLocalizer["refuel:object:no_fuel", new {Object = tank.name}]);
+                if (interactable is InteractableGenerator generator)
+                {
+                    if (!generator.isRefillable)
+                    {
+                        throw new UserFriendlyException(m_StringLocalizer["refuel:object:no_fuel", new { Object = generator.name }]);
+                    }
 
-                BarricadeManager.updateTank(raycast.transform, tank.capacity);
-                await uPlayer.PrintMessageAsync(m_StringLocalizer["refuel:object:tank"]);
-                return;
+                    generator.askFill(generator.capacity);
+                    BarricadeManager.sendFuel(raycast.transform, generator.fuel);
+
+                    await PrintAsync(m_StringLocalizer["refuel:object:generator"]);
+                    return;
+                }
+                else if (interactable is InteractableOil oil)
+                {
+                    if (!oil.isRefillable)
+                    {
+                        throw new UserFriendlyException(m_StringLocalizer["refuel:object:no_fuel", new { Object = oil.name }]);
+                    }
+
+                    oil.askFill(oil.capacity);
+                    BarricadeManager.sendFuel(raycast.transform, oil.fuel);
+
+                    await PrintAsync(m_StringLocalizer["refuel:object:oil"]);
+                    return;
+                }
+                else if (interactable is InteractableTank { source: ETankSource.FUEL } tank)
+                {
+                    if (!tank.isRefillable)
+                    {
+                        throw new UserFriendlyException(m_StringLocalizer["refuel:object:no_fuel", new { Object = tank.name }]);
+                    }
+
+                    BarricadeManager.updateTank(raycast.transform, tank.capacity);
+
+                    await PrintAsync(m_StringLocalizer["refuel:object:tank"]);
+                    return;
+                }
+                else if (interactable is InteractableObjectResource { objectAsset: { interactability: EObjectInteractability.FUEL } } @object)
+                {
+                    if (!@object.isRefillable)
+                    {
+                        throw new UserFriendlyException(m_StringLocalizer["refuel:object:no_fuel", new { Object = @object.name }]);
+                    }
+
+                    ObjectManager.updateObjectResource(interactable.transform, @object.capacity, true);
+
+                    // todo
+                    await PrintAsync(m_StringLocalizer["refuel:object:object"]);
+                    return;
+                }
             }
 
             throw new UserFriendlyException(m_StringLocalizer["refuel:none"]);
