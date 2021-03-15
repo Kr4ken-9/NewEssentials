afb46663fac9da7ed611d3fca4ca658ee0e298bd
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-03-15 16:57:23 +0700

    Add TPS command

---
+++ NewEssentials/Commands/CTPS.cs
@@ -0,0 +1,30 @@
+﻿using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Localization;
+using OpenMod.Core.Commands;
+using OpenMod.Core.Console;
+using OpenMod.Unturned.Commands;
+using SDG.Unturned;
+using System;
+
+namespace NewEssentials.Commands
+{
+    [Command("tps")]
+    [CommandDescription("Shows TPS")]
+    public class CTPS : UnturnedCommand
+    {
+        private readonly IStringLocalizer m_StringLocalizer;
+
+        public CTPS(IServiceProvider serviceProvider, IStringLocalizer stringLocalizer) : base(serviceProvider)
+        {
+            m_StringLocalizer = stringLocalizer;
+        }
+
+        protected override UniTask OnExecuteAsync()
+        {
+            var tps = Provider.debugTPS;
+
+            return PrintAsync(m_StringLocalizer["tps:tps", new { Ticks = tps, IsConsole = Context.Actor is ConsoleActor }])
+                .AsUniTask(false);
+        }
+    }
+}
