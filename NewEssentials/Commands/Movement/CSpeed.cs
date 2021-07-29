0aec378badb567b5d5fa24392f6e68f9591e1a6a
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-07-30 04:38:22 +0700

    Add speed command

---
+++ NewEssentials/Commands/Movement/CSpeed.cs
@@ -0,0 +1,43 @@
+﻿using Cysharp.Threading.Tasks;
+using OpenMod.API.Permissions;
+using OpenMod.Core.Commands;
+using OpenMod.Core.Permissions;
+using OpenMod.Unturned.Commands;
+using OpenMod.Unturned.Users;
+using System;
+
+namespace NewEssentials.Commands.Movement
+{
+    [Command("speed")]
+    [CommandDescription("Change your or other player movement speed. 0 = Freeze, 1 = Normal")]
+    [CommandSyntax("[user] <speed>")]
+    [RegisterCommandPermission("other", Description = "Allows to change speed of other player")]
+    public class CSpeed : UnturnedCommand
+    {
+        public CSpeed(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        protected override async UniTask OnExecuteAsync()
+        {
+            var targetActor = Context.Parameters.Count == 2
+                ? await Context.Parameters.GetAsync<UnturnedUser>(0)
+                : (UnturnedUser)Context.Actor;
+
+            if (targetActor != Context.Actor && await CheckPermissionAsync("other") != PermissionGrantResult.Grant)
+            {
+                throw new NotEnoughPermissionException(Context, "other");
+            }
+
+            if (targetActor == null)
+            {
+                return;
+            }
+
+            var speed = await Context.Parameters.GetAsync<float>(Context.Parameters.Count - 1);
+
+            await UniTask.SwitchToMainThread();
+            targetActor.Player.Player.movement.sendPluginSpeedMultiplier(speed);
+        }
+    }
+}
